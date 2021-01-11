4c6b16700c149a0fedf22f25db772f1cc5dd4df1
Daniel Alejandro Gil Araujo <77291820+DanielGil-2004@users.noreply.github.com>
2021-01-11 13:35:37 -0400

    Add files via upload

---
+++ Javascript-products-app/Javascript.js
@@ -0,0 +1,52 @@
+class Product {
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class UI{
+    addProduct(Product){
+const productList = document.getElementById('product-list')
+const element = document.createElement('div');
+element.innerHTML = '
+    <div class="card text-center mb-4">
+        <div class="card body">
+            <strong>Product name </strong>: $(product.name)
+            <strong>Product price </strong>: $(product.price)
+            <strong>Product year </strong>: $(product.year)
+        </div>
+    </div>
+';
+productList.appendChild(element);
+
+
+    }
+
+    deleteProduct(){
+
+    }
+
+    showMessage(){
+
+    }
+}
+
+//DOM EVENTS
+document.getElementById('product-form')
+    .addEventListener('submit', function(e){
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
+
+    const product = new Product(name, price, year)
+
+    const ui = new UI();
+
+    ui.addProduct();
+
+
+
+e.preventDefault
+})
